f1c3b13e7245f5714c401253a676300c91cd004d
Pedro Pascoal <ppascoal@visor.ai>
2025-05-17 19:29:16 +0100

    Add artifact

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,6 @@
-// src/main/kotlin/Main.kt
 import framework.GetJson
 import controllers.Controller
 
 fun main() {
-    // injeta seu controller e dispara na  porta 8080
     GetJson(Controller::class).start(8080)
 }
